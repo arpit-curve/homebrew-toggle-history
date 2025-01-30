df5db30c1bacd419681752ac91b7a409926b596f
Administrator <admin@example.com>
2025-01-30 16:10:31 +0530

    fix in path of repo

--- Formula/toggle-history.rb
+++ Formula/toggle-history.rb
@@ -1,7 +1,7 @@
 class ToggleHistory < Formula
   desc "Interactive script to enable or disable command history tracking"
-  homepage "https://github.com/arpit-curve/shell-history-manager"
-  url "https://github.com/arpit-curve/shell-history-manager/archive/v1.0.0.tar.gz"
+  homepage "https://github.com/arpit-curve/homebrew-toggle-history"
+  url "https://github.com/arpit-curve/homebrew-toggle-history/archive/v1.0.0.tar.gz"
   sha256 "ccfa34795c994014aa10c5266c66061fdaef77d4efdcdd56376b0943ebad0d3c"
   license "MIT"
 
